138d73db658addac957d138bef8b74baa6aa8c38
Uday Annavarapu <Udaypats2005@gmail.com>
2024-09-30 18:25:06 -0400

    Designed Rough Draft of hunt_alone_view.dart

--- client/lib/views/new_screens/hunt_alone_view.dart
+++ client/lib/views/new_screens/hunt_alone_view.dart
@@ -1,39 +1,130 @@
 import 'package:flutter/material.dart';
 import 'package:praxis_afterhours/views/new_screens/start_hunt_view.dart';
 
-class HuntAloneView extends StatelessWidget {
+class HuntAloneView extends StatefulWidget {
   const HuntAloneView({super.key});
+
+  @override
+  _HuntAloneViewState createState() => _HuntAloneViewState();
+}
+
+class _HuntAloneViewState extends State<HuntAloneView> {
+  final TextEditingController _teamNameController = TextEditingController();
+
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Hunt Alone Screen'),
+          title: const Text('Hunt Alone'),
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const TextField(
-                decoration: InputDecoration(
-                  border: UnderlineInputBorder(),
-                  labelText: 'Enter your team name',
+        body: Stack(
+          children: [
+            Container(
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/praxisbackground.jpg'),
+                  fit: BoxFit.cover,
                 ),
               ),
-              const SizedBox(height: 20), // Add space between buttons
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const StartHuntView()),
-                  );
-                },
-                child: const Text('Go to Start Hunt Screen'),
+            ),
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          colors: [
+                            Colors.grey[800]!,
+                            Colors.red.withOpacity(0.1),
+                            Colors.grey[800]!,
+                          ],
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                          stops: const [0.25, 0.5, .75],
+                        ),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      padding: const EdgeInsets.all(10.0),
+                      child: const Text(
+                        'Explore Praxis',
+                        style:
+                            TextStyle(color: Colors.white, fontSize: 16),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20.0),
+                    child: Divider(
+                      color: Colors.white,
+                      thickness: 2,
+                    ),
+                  ),
+                  const SizedBox(height: 1),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20.0),
+                    child: Text(
+                      'You have chosen to hunt alone, please enter your team name:',
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                      textAlign: TextAlign.left,
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                    child: TextField(
+                      controller: _teamNameController,
+                      style: const TextStyle(color: Colors.grey),
+                      decoration: const InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
+                        hintText: 'Enter your team name here:',
+                        hintStyle: TextStyle(color: Colors.grey),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(12)),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.transparent),
+                          borderRadius: BorderRadius.all(Radius.circular(12)),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.green),
+                          borderRadius: BorderRadius.all(Radius.circular(12)),
+                        ),
+                      ),
+                      
+                    ),
+                  ),
+                  const SizedBox(height: 50),
+                  SizedBox(
+                    width: 200,
+                    height: 60,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color.fromARGB(255, 34, 120, 34),
+                        foregroundColor: Colors.white,
+                        textStyle: const TextStyle(fontSize: 27),
+                      ),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const StartHuntView()),
+                        );
+                      },
+                      child: const Text('Continue'),
+                    ),
+                  ),
+                ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
