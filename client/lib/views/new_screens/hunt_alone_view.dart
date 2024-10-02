e3e9a5bfd3b73f2d9d6345bc5880301e138497fe
Uday Annavarapu <Udaypats2005@gmail.com>
2024-10-02 17:53:43 -0400

    Added App Bar, Gradients, and TextField Functions

--- client/lib/views/new_screens/hunt_alone_view.dart
+++ client/lib/views/new_screens/hunt_alone_view.dart
@@ -1,132 +1,210 @@
 import 'package:flutter/material.dart';
 import 'package:praxis_afterhours/views/new_screens/start_hunt_view.dart';
+import 'package:praxis_afterhours/styles/app_styles.dart';
+
 
 class HuntAloneView extends StatefulWidget {
-  const HuntAloneView({super.key});
+ final String teamName;
+ const HuntAloneView({super.key, required this.teamName});
 
-  @override
-  _HuntAloneViewState createState() => _HuntAloneViewState();
+
+ @override
+ _HuntAloneViewState createState() => _HuntAloneViewState();
 }
+
 
 class _HuntAloneViewState extends State<HuntAloneView> {
-  final TextEditingController _teamNameController = TextEditingController();
+ late TextEditingController _teamNameController;
+ late FocusNode _focusNode;
+ bool _isEditing = false;
 
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Hunt Alone'),
-        ),
-        body: Stack(
-          children: [
-            Container(
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage('assets/images/praxisbackground.jpg'),
-                  fit: BoxFit.cover,
-                ),
-              ),
-            ),
-            Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                    child: Container(
-                      decoration: BoxDecoration(
-                        gradient: LinearGradient(
-                          colors: [
-                            Colors.grey[800]!,
-                            Colors.red.withOpacity(0.1),
-                            Colors.grey[800]!,
-                          ],
-                          begin: Alignment.topCenter,
-                          end: Alignment.bottomCenter,
-                          stops: const [0.25, 0.5, .75],
-                        ),
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                      padding: const EdgeInsets.all(10.0),
-                      child: const Text(
-                        'Explore Praxis',
-                        style:
-                            TextStyle(color: Colors.white, fontSize: 16),
-                        textAlign: TextAlign.center,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 10),
-                  const Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 20.0),
-                    child: Divider(
-                      color: Colors.white,
-                      thickness: 2,
-                    ),
-                  ),
-                  const SizedBox(height: 1),
-                  const Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 20.0),
-                    child: Text(
-                      'You have chosen to hunt alone, please enter your team name:',
-                      style: TextStyle(color: Colors.white, fontSize: 20),
-                      textAlign: TextAlign.left,
-                    ),
-                  ),
-                  const SizedBox(height: 10),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                    child: TextField(
-                      controller: _teamNameController,
-                      style: const TextStyle(color: Colors.grey),
-                      decoration: const InputDecoration(
-                        filled: true,
-                        fillColor: Colors.white,
-                        hintText: 'Enter your team name here:',
-                        hintStyle: TextStyle(color: Colors.grey),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(12)),
-                        ),
-                        enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.transparent),
-                          borderRadius: BorderRadius.all(Radius.circular(12)),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.green),
-                          borderRadius: BorderRadius.all(Radius.circular(12)),
-                        ),
-                      ),
-                      
-                    ),
-                  ),
-                  const SizedBox(height: 50),
-                  SizedBox(
-                    width: 200,
-                    height: 60,
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color.fromARGB(255, 34, 120, 34),
-                        foregroundColor: Colors.white,
-                        textStyle: const TextStyle(fontSize: 27),
-                      ),
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const StartHuntView()),
-                        );
-                      },
-                      child: const Text('Continue'),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
+ @override
+ void initState() {
+   super.initState();
+   _teamNameController = TextEditingController(text: widget.teamName);
+   _focusNode = FocusNode();
+
+
+   _focusNode.addListener(() {
+     setState(() {
+       _isEditing = _focusNode.hasFocus;
+     });
+   });
+ }
+
+
+ @override
+ void dispose() {
+   _teamNameController.dispose();
+   _focusNode.dispose();
+   super.dispose();
+ }
+
+
+ void _unfocusTextField() {
+   if (_focusNode.hasFocus) {
+     _focusNode.unfocus();
+   }
+ }
+
+
+ @override
+ Widget build(BuildContext context) {
+   return MaterialApp(
+     home: GestureDetector(
+       onTap: _unfocusTextField,
+       child: Scaffold(
+         appBar: AppStyles.appBarStyle("Hunt Alone Screen", context),
+         body: DecoratedBox(
+           decoration: AppStyles.backgroundStyle,
+           child: Center(
+             child: Column(
+               mainAxisAlignment: MainAxisAlignment.center,
+               children: [
+                 Container(
+                   height: 150,
+                   width: 350,
+                   padding: const EdgeInsets.all(16),
+                   decoration: AppStyles.infoBoxStyle,
+                   child: Column(
+                     children: [
+                       Row(
+                         children: [
+                           Text(
+                             "Explore Praxis",
+                             textAlign: TextAlign.left,
+                             style: AppStyles.logisticsStyle,
+                           ),
+                         ],
+                       ),
+                       const SizedBox(height: 20),
+                       Row(
+                         children: [
+                           Icon(Icons.location_pin, color: Colors.white),
+                           Text(
+                             "The Greene Turtle (in-person only)",
+                             style: AppStyles.logisticsStyle,
+                           ),
+                         ],
+                       ),
+                       const SizedBox(height: 20),
+                       Row(
+                         children: [
+                           Icon(Icons.calendar_month, color: Colors.white),
+                           Text(
+                             "01/30/2024 at 8:30pm",
+                             style: AppStyles.logisticsStyle,
+                           ),
+                         ],
+                       ),
+                     ],
+                   ),
+                 ),
+                 const SizedBox(height: 20),
+                 Text(
+                   "You are currently hunting alone as: ${_teamNameController.text}",
+                   style: AppStyles.logisticsStyle,
+                 ),
+                 const SizedBox(width: 350, child: Divider(thickness: 2)),
+                 Container(
+                   height: 75,
+                   width: 350,
+                   padding: const EdgeInsets.all(16),
+                   decoration: AppStyles.infoBoxStyle,
+                   child: Row(
+                     children: [
+                       Icon(Icons.person, color: Colors.white),
+                       const SizedBox(width: 5),
+                       SizedBox(
+                         width: 205,
+                         child: TextField(
+                           controller: _teamNameController,
+                           focusNode: _focusNode,
+                           decoration: InputDecoration(
+                             suffixIcon: Icon(Icons.edit, color: Colors.white),
+                             border: UnderlineInputBorder(
+                               borderRadius: BorderRadius.circular(10),
+                               borderSide: BorderSide(color: Colors.white),
+                             ),
+                             hintText: _isEditing ? null : widget.teamName,
+                             labelStyle: const TextStyle(
+                                 color: Colors.white, fontSize: 14),
+                             filled: true,
+                             fillColor: Colors.grey,
+                           ),
+                           style: const TextStyle(color: Colors.white),
+                         ),
+                       ),
+                       const SizedBox(width: 5),
+                       Icon(Icons.lock, color: Colors.white),
+                       const SizedBox(width: 5),
+                       Text(
+                         "(Solo)",
+                         style: AppStyles.logisticsStyle,
+                       ),
+                     ],
+                   ),
+                 ),
+                 const SizedBox(height: 20),
+                 const SizedBox(width: 350, child: Divider(thickness: 2)),
+                 Container(
+                   width: 350,
+                   padding: const EdgeInsets.all(16),
+                   decoration: AppStyles.infoBoxStyle,
+                   child: const Text(
+                     "There are 3 teams and one solo team currently hunting. Select \"Start Hunt\" when you are ready to begin.",
+                     style: TextStyle(
+                         fontSize: 20,
+                         color: Colors.white,
+                         fontFamily: 'InriaSerif'),
+                   ),
+                 ),
+                 const SizedBox(height: 50),
+                 Container(
+                   height: 50,
+                   width: 175,
+                   decoration: AppStyles.confirmButtonStyle,
+                   child: ElevatedButton(
+                     onPressed: () {
+                       Navigator.push(
+                         context,
+                         MaterialPageRoute(
+                             builder: (context) => const StartHuntView()),
+                       );
+                     },
+                     style: AppStyles.elevatedButtonStyle,
+                     child: const Text('Start Hunt',
+                         style: TextStyle(fontWeight: FontWeight.bold)),
+                   ),
+                 ),
+                 const SizedBox(height: 50),
+                 Container(
+                   height: 50,
+                   width: 175,
+                   decoration: AppStyles.cancelButtonStyle,
+                   child: ElevatedButton(
+                     onPressed: () {
+                       Navigator.push(
+                         context,
+                         MaterialPageRoute(
+                             builder: (context) => const StartHuntView()),
+                       );
+                     },
+                     style: AppStyles.elevatedButtonStyle,
+                     child: const Text('Delete Team',
+                         style: TextStyle(fontWeight: FontWeight.bold)),
+                   ),
+                 ),
+               ],
+             ),
+           ),
+         ),
+       ),
+     ),
+   );
+ }
 }
+
+
+
