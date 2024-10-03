ecf626115d9442640c3f896d369b6748a298d872
Tristan Tsang <tristantsang9786@gmail.com>
2024-10-02 22:42:19 -0400

    Updated Join A Team Screen To New Style
    - Updated screen to more closely reflect stylistic
    theme of app

--- client/lib/views/new_screens/join_a_team_view.dart
+++ client/lib/views/new_screens/join_a_team_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:praxis_afterhours/styles/app_styles.dart';
 
 class JoinATeamView extends StatelessWidget {
   const JoinATeamView({super.key});
@@ -7,10 +8,9 @@ class JoinATeamView extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Join A Team Screen'),
-        ),
-        body: const TeamList(),
+        appBar: AppStyles.appBarStyle("Join A Team", context),
+        body: DecoratedBox(
+            decoration: AppStyles.backgroundStyle, child: const TeamList()),
         // body: const Center(
         //   child: Text(
         //     'Join A Team Screen, waiting for team leader to start hunt...',
@@ -91,72 +91,91 @@ class _TeamTileState extends State<TeamTile> {
   @override
   Widget build(BuildContext context) {
     return Card(
-      margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-      child: ExpansionTile(
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(
-              widget.teamName,
-              style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            Row(
+        margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+        child: DecoratedBox(
+          decoration: AppStyles.infoBoxStyle,
+          child: ExpansionTile(
+            iconColor: Colors.white,
+            collapsedIconColor: Colors.white,
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                if (widget.isLocked)
-                  const Icon(Icons.lock, color: Colors.grey),
-                Text("(${widget.members.length}/4)",
-                    style: const TextStyle(fontSize: 16)),
-              ],
-            ),
-          ],
-        ),
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: widget.members.asMap().entries.map((entry) {
-              int index = entry.key;
-              String member = entry.value;
-              return ListTile(
-                leading: const Icon(Icons.person),
-                title: Row(
+                Text(
+                  widget.teamName,
+                  style: AppStyles.logisticsStyle.copyWith(fontSize: 24),
+                ),
+                Row(
                   children: [
-                    Text(member),
-                    if (index == 0) // Add crown to the first member
-                      const Padding(
-                        padding: EdgeInsets.only(left: 8.0),
-                        child: Icon(Icons.star, color: Colors.amber),
-                      ),
+                    if (widget.isLocked)
+                      const Icon(Icons.lock, color: Colors.white),
+                    Text(
+                      "(${widget.members.length}/4)",
+                      style: AppStyles.logisticsStyle,
+                    ),
                   ],
                 ),
-              );
-            }).toList(),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ElevatedButton(
-              onPressed: widget.isLocked
-                  ? null // Disable button if team is locked
-                  : () {
-                      // Join team functionality
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(content: Text('Joined ${widget.teamName}')),
-                      );
-                      // *****************
-                      // setState(() {
-                      //   widget.members.add("CURRENT USER NAME");
-                      // });
-                      // **************
-                    },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: widget.isLocked
-                    ? Colors.grey
-                    : Colors.redAccent, // Button color
-              ),
-              child: const Text('Join Team!'),
+              ],
             ),
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: widget.members.asMap().entries.map((entry) {
+                  int index = entry.key;
+                  String member = entry.value;
+                  return Column(children: [
+                    Divider(
+                        color: Colors.grey,
+                        indent: 15,
+                        endIndent: 15,
+                        height: 1),
+                    ListTile(
+                      leading: const Icon(Icons.person, color: Colors.white),
+                      title: Row(
+                        children: [
+                          Text(
+                            member,
+                            style: AppStyles.logisticsStyle,
+                          ),
+                          if (index == 0) // Add crown to the first member
+                            const Padding(
+                              padding: EdgeInsets.only(left: 8.0),
+                              child: Icon(Icons.star, color: Colors.amber),
+                            ),
+                        ],
+                      ),
+                    )
+                  ]);
+                }).toList(),
+              ),
+              if (!widget.isLocked)
+                Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      height: 50,
+                      width: 175,
+                      decoration: AppStyles.cancelButtonStyle,
+                      child: ElevatedButton(
+                        onPressed: widget.isLocked
+                            ? null // Disable button if team is locked
+                            : () {
+                                // Join team functionality
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  SnackBar(
+                                      content:
+                                          Text('Joined ${widget.teamName}')),
+                                );
+                                // *****************
+                                // setState(() {
+                                //   widget.members.add("CURRENT USER NAME");
+                                // });
+                                // **************
+                              },
+                        style: AppStyles.elevatedButtonStyle,
+                        child: const Text('Join Team!'),
+                      ),
+                    )),
+            ],
           ),
-        ],
-      ),
-    );
+        ));
   }
 }
