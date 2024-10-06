a6f9df942fa181ed5df48cfb4a804a37d8e54e07
Uday Annavarapu <Udaypats2005@gmail.com>
2024-10-06 14:13:45 -0400

    Added change team name functionality to my_team_create_view

--- client/lib/views/new_screens/my_team_create_view.dart
+++ client/lib/views/new_screens/my_team_create_view.dart
@@ -1,102 +1,173 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:praxis_afterhours/apis/hunts_api.dart' as hunts_api;
 import 'package:praxis_afterhours/views/new_screens/challenge_view.dart';
-import 'package:praxis_afterhours/views/new_screens/start_hunt_view.dart';
 import 'package:praxis_afterhours/styles/app_styles.dart';
 
-class MyTeamCreateView extends StatelessWidget {
+class MyTeamCreateView extends StatefulWidget {
   final String teamName;
   final String individualName;
 
-  const MyTeamCreateView(
-      {super.key, required this.teamName, required this.individualName});
+  const MyTeamCreateView({
+    Key? key,
+    required this.teamName,
+    required this.individualName,
+  }) : super(key: key);
+
+  @override
+  _MyTeamCreateViewState createState() => _MyTeamCreateViewState();
+}
+
+class _MyTeamCreateViewState extends State<MyTeamCreateView> {
+  late TextEditingController _teamNameController;
+  late FocusNode _focusNode;
+  bool _isEditing = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _teamNameController = TextEditingController(text: widget.teamName);
+    _focusNode = FocusNode();
+    _focusNode.addListener(() {
+      setState(() {
+        _isEditing = _focusNode.hasFocus;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _teamNameController.dispose();
+    _focusNode.dispose();
+    super.dispose();
+  }
+
+  void _unfocusTextField() {
+    if (_focusNode.hasFocus) {
+      _focusNode.unfocus();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppStyles.appBarStyle("My Team", context),
-      body: DecoratedBox(
-        decoration: BoxDecoration(
-          image: DecorationImage(
+    return GestureDetector(
+      onTap: _unfocusTextField,  // Unfocus when tapping outside the TextField
+      child: Scaffold(
+        appBar: AppStyles.appBarStyle("My Team", context),
+        body: DecoratedBox(
+          decoration: BoxDecoration(
+            image: DecorationImage(
               image: AssetImage("images/cracked_background.jpg"),
-              fit: BoxFit.cover),
-        ),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                "Team Name",
-                style: AppStyles.logisticsStyle,
-              ),
-              const SizedBox(width: 350, child: Divider(thickness: 2)),
-              Text(
-                teamName,
-                style: AppStyles.logisticsStyle,
-              ),
-              const SizedBox(height: 20),
-              Container(
-                height: 75,
-                width: 325,
-                padding: const EdgeInsets.all(16),
-                decoration: AppStyles.infoBoxStyle,
-                child: Row(
-                  children: [
-                    Icon(FontAwesomeIcons.crown, color: Color(0xFFFFD700)),
-                    const SizedBox(width: 5),
-                    Icon(Icons.person, color: Colors.green),
-                    const SizedBox(width: 5),
-                    SizedBox(
-                      child: Text(
-                        individualName,
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Team Name",
+                  style: AppStyles.logisticsStyle,
+                ),
+                const SizedBox(width: 350, child: Divider(thickness: 2)),
+                Container(
+                  height: 75,
+                  width: 325,
+                  padding: const EdgeInsets.all(16),
+                  decoration: AppStyles.infoBoxStyle,
+                  child: Row(
+                    children: [
+                      Icon(Icons.person, color: Colors.white),
+                      const SizedBox(width: 5),
+                      SizedBox(
+                        width: 250,
+                        child: TextField(
+                          controller: _teamNameController,
+                          focusNode: _focusNode,
+                          decoration: InputDecoration(
+                            suffixIcon: Icon(Icons.edit, color: Colors.white),
+                            border: UnderlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: BorderSide(color: Colors.white),
+                            ),
+                            hintText: _isEditing ? null : widget.teamName,
+                            labelStyle: const TextStyle(
+                                color: Colors.white, fontSize: 14),
+                            filled: true,
+                            fillColor: Colors.grey,
+                          ),
+                          style: const TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Container(
+                  height: 75,
+                  width: 325,
+                  padding: const EdgeInsets.all(16),
+                  decoration: AppStyles.infoBoxStyle,
+                  child: Row(
+                    children: [
+                      Icon(FontAwesomeIcons.crown, color: Color(0xFFFFD700)),
+                      const SizedBox(width: 5),
+                      Icon(Icons.person, color: Colors.green),
+                      const SizedBox(width: 5),
+                      SizedBox(
+                        child: Text(
+                          widget.individualName,
+                          style: AppStyles.logisticsStyle,
+                        ),
+                      ),
+                      Spacer(),
+                      Text(
+                        "(Me)",
                         style: AppStyles.logisticsStyle,
                       ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Container(
+                  height: 50,
+                  width: 175,
+                  decoration: AppStyles.confirmButtonStyle,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const ChallengeView(),
+                        ),
+                      );
+                    },
+                    style: AppStyles.elevatedButtonStyle,
+                    child: const Text(
+                      'Start Hunt',
+                      style: TextStyle(fontWeight: FontWeight.bold),
                     ),
-                    Spacer(),
-                    Text(
-                      "(Me)",
-                      style: AppStyles.logisticsStyle,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Container(
+                  height: 50,
+                  width: 175,
+                  decoration: AppStyles.cancelButtonStyle,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(content: Text('Deleted team')),
+                      );
+                    },
+                    style: AppStyles.elevatedButtonStyle,
+                    child: const Text(
+                      'Delete Team',
+                      style: TextStyle(fontWeight: FontWeight.bold),
                     ),
-                  ],
+                  ),
                 ),
-              ),
-              const SizedBox(height: 20),
-              Container(
-                height: 50,
-                width: 175,
-                decoration: AppStyles.confirmButtonStyle,
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          //builder: (context) => const StartHuntView()),
-                          builder: (context) => const ChallengeView()),
-                    );
-                  },
-                  style: AppStyles.elevatedButtonStyle,
-                  child: const Text('Start Hunt',
-                      style: TextStyle(fontWeight: FontWeight.bold)),
-                ),
-              ),
-              const SizedBox(height: 20),
-              Container(
-                height: 50,
-                width: 175,
-                decoration: AppStyles.cancelButtonStyle,
-                child: ElevatedButton(
-                  onPressed: () {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(content: Text('Deleted team')),
-                    );
-                  },
-                  style: AppStyles.elevatedButtonStyle,
-                  child: const Text('Delete Team',
-                      style: TextStyle(fontWeight: FontWeight.bold)),
-                ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
